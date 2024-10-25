3f7995a9bfdaeb6161de79bc2600b7ae68f6b30a
Juserna28 <seserna1028@gmail.com>
2024-10-25 10:58:46 -0500

    Creacion de Cru para las categorias
    Se creo la funcion de actualizar lacategoria segun su id

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,7 @@ import Category from '../Models/Category.js';
 
 const router = express.Router();
 
-// Crear una categoría
+// Crear una categoría (Post)
 router.post('/', async (req, res) => {
     const { name } = req.body; 
     //Validar que etnga name
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Obtener todas las categorías
+// Obtener todas las categorías (Get)
 router.get('/', async (req, res) => {
     try {
         const categories = await Category.findAll();
@@ -29,5 +29,33 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: 'Error al obtener categorías', error: error.message });
     }
 });
+
+// Actualizar una categoría (Put)
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    // Validación
+    if (!name) {
+        return res.status(400).json({ message: 'El campo name no puede estar vacio.' });
+    }
+
+    try {
+        const [updated] = await Category.update({ name }, {
+            where: { id }
+        });
+
+        if (updated) {
+            const updatedCategory = await Category.findByPk(id);
+            res.status(200).json(updatedCategory);
+        } else {
+            res.status(404).json({ message: 'Categoría no encontrada.' });
+        }
+    } catch (error) {
+        console.error('Error al actualizar la categoría:', error);
+        res.status(500).json({ message: 'Error al actualizar la categoría', error: error.message });
+    }
+});
+
 
 export default router;
